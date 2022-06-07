6ec235bdd9e4b79f0adf8f0ab6f8323998568a23
Keightly Mabasa <keightlymabasa@gmail.com>
2022-06-06 17:52:03 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,20 +1,8 @@
 #include <stdio.h>
 
+        
 
-
-/**
-
- * main - entry point
-
- *
-
- * Description - prints "Programming is like building a multilingual puzzle
-
- *
-
- * Return: 0
-
- **/
+        
 
 int main(void)
 
