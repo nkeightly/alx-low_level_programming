13f67bf68715a1130bfc7c1b94d51f84d212210e
nkeightly <104975766+nkeightly@users.noreply.github.com>
2022-06-08 03:32:59 +0200

    2. 10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,1 +1,27 @@
 #include "main.h"
+
+/**
+* print_alphabet_x10 - a void function
+*
+* Description - prints the english alphabets 10X.
+*
+* Return: void
+**/
+void print_alphabet_x10(void)
+{
+	int counter = 0;
+
+	while (counter < 10)
+	{
+		int characta = 97;
+
+		while (characta < 123)
+		{
+			_putchar(characta);
+			characta++;
+		}
+
+		_putchar('\n');
+		counter++;
+	}
+}
