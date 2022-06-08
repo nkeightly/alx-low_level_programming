ce37cb1aaa30c1f887cef53c2b411f4cd20c5462
nkeightly <104975766+nkeightly@users.noreply.github.com>
2022-06-08 02:09:56 +0200

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
-
+/**
+ * main - entry point
+ *
+ * Description - prints "Programming is like building a multilingual puzzle
+ *
+ * Return: 0
+ **/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
