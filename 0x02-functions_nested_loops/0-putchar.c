61b65aab554132c18e7184dfd713c3008ea9512c
nkeightly <104975766+nkeightly@users.noreply.github.com>
2022-06-08 03:24:45 +0200

    Update

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,23 +1,22 @@
 #include "main.h"
 
-
 /**
- * main - Entry point
- *
- * Return: nothing, return void
- */
-
+* main - entry point
+* Description - prints "_putchar"
+* Return: 0
+**/
 int main(void)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
+	char str[9] = "_putchar";
+	int i = 0;
 
-return (0);
+	while (i < 8)
+	{
+		_putchar(str[i]);
+		i++;
+	}
+
+	_putchar('\n');
+
+	return (0);
 }
