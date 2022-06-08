0d9e2a2a82a2359affbfdc28feff754dba661c2c
nkeightly <104975766+nkeightly@users.noreply.github.com>
2022-06-08 02:12:29 +0200

    5. Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
- int main(void)
+/**
+ * main - Entry point
+ *
+ * Description: prints "with proper grammar, but the outcome is a piece of
+ * art," with a newline.
+ *
+ * Return: 0
+ */
+int main(void)
 {
-  printf("with proper grammar, but the outcome is a piece of art,\n");
-  return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
