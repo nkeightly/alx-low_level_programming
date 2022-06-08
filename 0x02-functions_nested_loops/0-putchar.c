2a45d5b6f375f951bd43ba30a4c1e1ffcbaf222e
nkeightly <104975766+nkeightly@users.noreply.github.com>
2022-06-08 02:47:36 +0200

    0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,24 @@
-#include "main.h"                                                                                    
+#include "main.h"
 
->                                                                                                                                                        
+/**
+* main - entry point
+*
+* Description - prints "_putchar"
+*
+* Return: 0
+**/
+int main(void)
+{
+	char str[9] = "_putchar";
+	int i = 0;
 
-> 
+	while (i < 8)
+	{
+		_putchar(str[i]);
+		i++;
+	}
+
+	_putchar('\n');
+
+	return (0);
+}
