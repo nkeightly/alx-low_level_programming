129e5337b3d4f9c9e91e80f14779c65bdf252e88
nkeightly <104975766+nkeightly@users.noreply.github.com>
2022-06-08 03:29:47 +0200

    1. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,1 +1,19 @@
 #include "main.h"
+
+/**
+* print_alphabet - a void function
+* Description - prints the english alphabets
+* Return: void
+**/
+void print_alphabet(void)
+{
+	int counter = 97;
+
+	while (counter < 123)
+	{
+		_putchar(counter);
+		counter++;
+	}
+
+	_putchar('\n');
+}
