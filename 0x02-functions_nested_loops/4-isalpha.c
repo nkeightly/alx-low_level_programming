170f5ef099f01312797829aa8f28d5cb0b9138f7
nkeightly <104975766+nkeightly@users.noreply.github.com>
2022-06-08 03:41:54 +0200

    4. isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,1 +1,21 @@
-hhh
+#include "main.h"
+
+/**
+* _isalpha - alphabet checker
+*
+* @c: ascii character
+*
+* Description - checks for alphabet
+*
+* Return: 1 | 0 depending on c
+**/
+int _isalpha(int c)
+{
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+	{
+		return (1);
+	} else
+	{
+		return (0);
+	}
+}
