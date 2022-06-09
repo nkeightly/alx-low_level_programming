f72db076c242ffebd546a559f333b0469c9f495c
nkeightly <104975766+nkeightly@users.noreply.github.com>
2022-06-09 05:15:15 +0200

    Last digit

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,14 @@
+/**
+ * main - header file
+ *
+ */
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -5,7 +16,6 @@ int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
-int print_last_digit(int);
-void times_table(void);
-int add(int, int);
-void print_to_98(int n);
+int print_last_digit(int c);
+
+#endif
